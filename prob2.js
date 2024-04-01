349cbe45fae109c9fa52bcb9d9e5918503aeda1b
ThinkMuk <b2000kang@gmail.com>
2024-04-01 20:09:12 +0900

    강명묵:2번문제

--- prob2.js
+++ prob2.js
@@ -1,1 +1,12 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(n) {
+  let tmp = 1;
+  let ans = 1;
+  while (1) {
+    tmp *= ans;
+    if (tmp >= n) break;
+    ans++;
+  }
+  if (tmp > n) ans -= 1;
+  return ans;
+}
