652df79e1e60237060c4678f26306a24aa70310e
ThinkMuk <b2000kang@gmail.com>
2024-04-01 20:09:41 +0900

    강명묵:3번문제

--- prob3.js
+++ prob3.js
@@ -1,1 +1,18 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(arr) {
+  let answer = [];
+  let tmp = 2147483647;
+  let deleteAddress;
+  if (arr.length <= 1) return [-1];
+  else {
+    arr.map(function (num, idx) {
+      if (tmp > num) {
+        tmp = num;
+        deleteAddress = idx;
+      }
+    });
+    arr.splice(deleteAddress, 1);
+  }
+
+  return arr;
+}
